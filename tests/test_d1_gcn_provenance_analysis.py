0b7ab57f9049ca942f82f147d1bcff3f98b69eef
Peter <53446060+lizardpeter@users.noreply.github.com>
2026-09-19 15:37:56 -0400

    d1: regression-test terminal RGB packed channel slicing

--- tests/test_d1_gcn_provenance_analysis.py
+++ tests/test_d1_gcn_provenance_analysis.py
@@ -7,7 +7,7 @@ ROOT=Path(__file__).resolve().parents[1]
 sys.path.insert(0,str(ROOT/'tools'))
 
 import d1_gcn_cbuffer_usage_analyze as cb
-import d1_gcn_terminal_alpha_slice as al
+import d1_gcn_terminal_alpha_slice as al\nimport d1_gcn_terminal_rgb_slice as rgb
 
 
 class TestGCNProvenance(unittest.TestCase):
@@ -57,6 +57,42 @@ class TestGCNProvenance(unittest.TestCase):
                 {'texture_index':'0','channel':'w'}
             ])
             self.assertEqual(d['value_slice']['unknown_registers'],[])
+        finally:
+            if p.exists():p.unlink()
+
+    def test_terminal_compressed_rgb_channel_unpack(self):
+        text='\n'.join([
+            '/*000000000000: 00000000 */ image_sample    v[2:4], v[8:11], s[4:11], s[12:15] dmask:7',
+            '/*000000000004: 00000000 */ s_buffer_load_dwordx4 s[4:7], s[0:3], 0x8',
+            '/*000000000008: 00000000 */ v_mul_f32       v2, v2, s4',
+            '/*00000000000c: 00000000 */ v_mul_f32       v3, v3, s5',
+            '/*000000000010: 00000000 */ v_mul_f32       v4, v4, s6',
+            '/*000000000014: 00000000 */ v_mov_b32       v5, 0',
+            '/*000000000018: 00000000 */ v_cvt_pkrtz_f16_f32 v6, v2, v3',
+            '/*00000000001c: 00000000 */ v_cvt_pkrtz_f16_f32 v7, v4, v5',
+            '/*000000000020: 00000000 */ exp             mrt0, v6, v6, v7, v7 done compr vm',
+        ])
+        p=ROOT/'tests'/'_tmp_terminal_rgb_test.s'
+        try:
+            p.write_text(text+'\n')
+            image={'instructions':[{
+                'address':'000000000000','dmask_channels':'xyz',
+                'resources':[{'texture_index':3}],
+            }]}
+            cbuf={'loads':[{
+                'address':'000000000004','destination':[4,5,6,7],
+                'api_slot':0,'dword_indices':[8,9,10,11],
+            }]}
+            d=rgb.analyze('DEADBEEF',p,image,cbuf)
+            self.assertEqual(d['terminal_packed_rg_register'],'v6')
+            self.assertEqual(d['terminal_packed_ba_register'],'v7')
+            for ch,texch,dw in [('R','x',8),('G','y',9),('B','z',10)]:
+                s=d['channels'][ch]['value_slice']
+                self.assertEqual(s['texture_sample_channels'],[
+                    {'texture_index':'3','channel':texch}
+                ])
+                self.assertEqual(s['cbuffer_dwords'],{'0':[dw]})
+                self.assertEqual(s['unknown_registers'],[])
         finally:
             if p.exists():p.unlink()
 
